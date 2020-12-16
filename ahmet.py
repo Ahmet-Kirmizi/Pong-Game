1f6101fc376295ee240457b7d795bb3f890ce68f
Ahmet Kirmizi <62718773+ahmetk5113@users.noreply.github.com>
2020-12-16 20:14:44 +0300

    Add files via upload

---
+++ ahmet.py
@@ -0,0 +1,13 @@
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @ahmet kirmizi")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+# Main game loop
+while True:
+    wn.update()
